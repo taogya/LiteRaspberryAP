9b96abf3547fc67125236b38aacce169a336ad81
Taogya <132679709+taogya@users.noreply.github.com>
2023-07-29 11:40:11 +0900

    Develop (#1)
    * Update README.md
    
    * first commit.
    
    * fix standalone.
    
    * add service stop in install.sh.
    
    * Create hostapd.conf
    
    * Create dhcpcd.conf
    
    * Create br0
    
    * Create wlan0
    
    * Update README.md
    
    * Update install.sh
    
    add bridge

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # LiteRaspberryAP
 This set up Wi-Fi Access Point with Router or Bridge or Standalone on Raspberry Pi.
+
+# Installation
+## Clone Repository
+```sh
+$ sudo apt-get update
+$ sudo apt-get upgrade
+$ sudo apt-get dist-upgrade
+$ 
+$ mkdir -p ~/work && cd ~/work
+$ git clone https://github.com/taogya/LiteRaspberryAP.git
+$ cd LiteRaspberryAP
+$ chmod -R +x shells/
+```
+
+## Select Mode & Modify conf, install
+i/f below is for example eth0, wlan0, usb0, etc..
+### Bridge
+```
+                                     target
+                                     v      wlan0
++----------+   i/f +--------+ br0    +----+ <---> dev_1
+| internet | <---> | Bridge | <----> | AP | <---> :
++----------+       +--------+        +----+ <---> dev_n
+```
+modify conf below.
+  - [dhcpcd.conf](conf/bridge/dhcpcd.conf)
+  - [hostapd.conf](conf/bridge/hostapd.conf)
+
+modify interfaces below.
+  - [wlan0](conf/bridge/interfaces/wlan0)
+  - [br0](conf/bridge/interfaces/br0)
+
+install
+```sh
+sudo sh shells/install.sh conf/bridge
+```
+
+### Standalone
+```
+                   target
+                   v      wlan0
++----------+   i/f +----+ <---> dev_1
+| internet | <-x-> | AP | <---> :
++----------+       +----+ <---> dev_n
+```
+modify conf below.
+  - [dhcpcd.conf](conf/standalone/dhcpcd.conf)
+  - [hostapd.conf](conf/standalone/hostapd.conf)
+  - [dnsmasq.conf](conf/standalone/dnsmasq.conf)
+
+modify interfaces below.
+  - [wlan0](conf/standalone/interfaces/wlan0)
+
+install
+```sh
+sudo sh shells/install.sh conf/standalone
+```
+
+# Uninstallation
+```sh
+sudo sh shells/uninstall.sh
+```
---
+++ shells/install.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+EXE_ERROR=1
+FILE_NOT_EXISTS=2
+
+# ===== function =================================================="
+help() {
+    echo "===== Lite Raspberry AP Installer ============================="
+    echo "===== Usage ==================================================="
+    echo "Argument"
+    echo "  shells/install.sh <conf>"
+    echo "    <conf>: configuration path."
+    echo "ex. standalone"
+    echo "  -> modify files in conf/standalone"
+    echo "  $ pwd"
+    echo "  /path/to/LiteRaspberryAP"
+    echo "  $ sudo sh shells/install.sh conf/standalone"
+    echo "==============================================================="
+}
+
+add_interfaces() {
+    # $1: conf path
+    if ! ls "$1" > /dev/null 2>&1; then
+        return ${FILE_NOT_EXISTS}
+    fi
+    echo "===== add interfaces ====="
+    \cp -f "$1"/* /etc/network/interfaces.d/
+    chmod 644 /etc/network/interfaces.d/*
+    chown root:root /etc/network/interfaces.d/*
+    for DEV in "$1"/*
+    do
+        ip addr flush dev "$(basename "${DEV}")"
+    done
+    systemctl restart dhcpcd
+    systemctl restart networking
+}
+
+install_bridge() {
+    apt-get install -y bridge-utils
+    echo "bridge interfaces:"
+    brctl show
+}
+
+install_dhcpcd() {
+    # $1: conf path
+    if ! ls "$1" > /dev/null 2>&1; then
+        return ${FILE_NOT_EXISTS}
+    fi
+    echo "===== install dhcpcd ====="
+    systemctl stop dhcpcd
+
+    if ! ls /etc/dhcpcd.conf.org > /dev/null 2>&1; then
+        \cp -f /etc/dhcpcd.conf /etc/dhcpcd.conf.org
+    fi
+
+    \cp -f "$1" /etc/dhcpcd.conf
+    chmod 664 /etc/dhcpcd.conf
+    chown root:netdev /etc/dhcpcd.conf
+
+    systemctl enable dhcpcd
+    systemctl start dhcpcd
+}
+
+install_dnsmasq() {
+    if ! ls "$1" > /dev/null 2>&1; then
+        return ${FILE_NOT_EXISTS}
+    fi
+    echo "===== install dnsmasq ====="
+    apt-get install -y dnsmasq
+    systemctl stop dnsmasq
+
+    if ! ls /etc/dnsmasq.conf.org > /dev/null 2>&1; then
+        \cp -f /etc/dnsmasq.conf /etc/dnsmasq.conf.org
+    fi
+
+    \cp -f "$1" /etc/dnsmasq.conf
+    chmod 644 /etc/dnsmasq.conf
+    chown root:root /etc/dnsmasq.conf
+
+    systemctl enable dnsmasq
+    systemctl start dnsmasq
+}
+
+install_hostapd() {
+    # $1: conf path
+    if ! ls "$1" > /dev/null 2>&1; then
+        return ${FILE_NOT_EXISTS}
+    fi
+    echo "===== install hostapd ====="
+    apt-get install -y hostapd
+    systemctl stop hostapd
+
+    if ! ls /etc/hostapd/hostapd.conf.org > /dev/null 2>&1; then
+        \cp -f /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.org > /dev/null 2>&1
+    fi
+
+    \cp -f "$1" /etc/hostapd/hostapd.conf
+    chmod 660 /etc/hostapd/hostapd.conf
+    chown root:root /etc/hostapd/hostapd.conf
+
+    sed -i '/#DAEMON_CONF=""/a DAEMON_CONF="/etc/hostapd/hostapd.conf"' /etc/default/hostapd
+    if ! grep -e "ExecStartPre" /lib/systemd/system/hostapd.service > /dev/null 2>&1; then
+        sed -i "/ExecStart=/a ExecStartPre=/bin/sleep 10" /lib/systemd/system/hostapd.service
+    fi
+
+    systemctl unmask hostapd
+    systemctl enable hostapd
+    systemctl start hostapd
+}
+
+# ===== script =================================================="
+if [ "$(whoami)" != "root" ]; then
+    echo "===== please execute with root. ===="
+    exit ${EXE_ERROR}
+fi
+
+if [ "$#" != 1 ]; then
+    echo "===== invalid argument. ===="
+    help
+    exit ${EXE_ERROR}
+fi
+
+if ! ping -c 4 8.8.8.8 > /dev/null 2>&1; then
+    echo "===== can not connect to internet. ====";
+    exit ${EXE_ERROR}
+fi
+
+add_interfaces "$1"/interfaces
+install_bridge
+install_dhcpcd "$1"/dhcpcd.conf
+install_dnsmasq "$1"/dnsmasq.conf
+install_hostapd "$1"/hostapd.conf
+
+reboot now
